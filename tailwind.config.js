382da82fc41a4f3295671418e8590d5b41178494
markl2305 <markflord@me.com>
2025-11-10 18:38:51 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,jsx,ts,tsx,mdx}',
-    './components/**/*.{js,jsx,ts,tsx,mdx}',
-    './pages/**/*.{js,jsx,ts,tsx,mdx}'
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         brand: {
-          teal: '#279A92',
-          sage: '#96B1AD',
-          beige: '#F1E7D8',
-          'teal-dark': '#1f7e77',
-          'teal-light': '#32b5ab'
+          teal: "#279A92",
+          "teal-dark": "#1f7e77",
+          "teal-light": "#32b5ab",
+          sage: "#96B1AD",
+          beige: "#F1E7D8"
         }
       }
     }
